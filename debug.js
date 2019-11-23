8c8f34945805339cf1463800e88cff85149c06eb
William Kapke <william.kapke@gmail.com>
2019-11-23 18:03:02 +0000

    use namespaces

--- debug.js
+++ debug.js
@@ -4,7 +4,7 @@ const _debug = require('debug')
 const hex = (v) => v.toString(16).padStart(2, '0')
 _debug.formatters.h = (v) => v.length ? Array.prototype.map.call(v, b => hex(b)).join(' ') : hex(v)
 module.exports = {
-  info: _debug('VL53L1X_info'),
-  verbose: _debug('VL53L1X_verbose')
+  info: _debug('VL53L1X:info'),
+  verbose: _debug('VL53L1X:verbose')
 }
 
